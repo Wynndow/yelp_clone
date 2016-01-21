88fd2294080c0e50ca6cd88381a5f9bfb70ead92
Chris Wynne <chris.wynndow@gmail.com>
2016-01-21 16:47:49 +0000

    Swaps star colour

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -2,6 +2,6 @@ module ReviewsHelper
   def star_rating(rating)
     return rating unless rating.respond_to?(:round)
     remainder = 5 - rating
-    '☆' * rating.round + ('★' * remainder)
+    '★' * rating.round + ('☆' * remainder)
   end
 end
