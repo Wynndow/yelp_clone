04095f6d25eaa0342f828383c3fe2cda1d9d77ad
Chris Wynne <chris.wynndow@gmail.com>
2016-01-23 11:06:42 +0000

    Envokes DATABASE_URL for production database for heroku deployment

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -239,7 +239,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
 
-  config.omniauth :facebook, Rails.application.secrets.facebook_app_id, Rails.application.secrets.facebook_app_secret #callback_url: "http://localhost:3000"#, scope: 'user,public_repo'
+  config.omniauth :facebook, 539241252918690, ENV["YELP_FACEBOOK_APP_SECRET"] #callback_url: "http://localhost:3000"#, scope: 'user,public_repo'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
